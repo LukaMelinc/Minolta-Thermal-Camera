cfef869431b9c9efd94de68cdd5a1d1765708f02
LukaMelinc <luka.melinc1@gmail.com>
2024-04-26 11:27:34 +0200

    comments added

--- GUI_2.py
+++ GUI_2.py
@@ -2,9 +2,13 @@ import tkinter as tk
 import tkinter.font as tkFont
 from tkinter import ttk
 import tkinter.font as tkFont
+import datetime
 
 class App:
     def __init__(self, root):
+
+        # (0,0) točka sistema je zgoraj levo
+
         #setting title
         root.title("Main")
         #setting window size
@@ -17,7 +21,7 @@ class App:
         root.resizable(width=False, height=False)
 
                         # Create Canvas widget
-                # Draw a vertical line at x=380 from top to bottom
+                # Draw a vertical line at x=380 from top to bottogitm
         canvas = tk.Canvas(root)
         canvas.place(x=0, y=0, width=width, height=height)
 
@@ -29,17 +33,18 @@ class App:
         canvas.create_line(380, 200, width, 200, fill="black") # Horizontal line at y=200 from x=380 to the right edge
         canvas.create_line(0, 390, width, 390, fill="black")   # Horizontal line at y=390 from left to right edge
 
-
+        # Connect button for kamera 2
         GButton_101=tk.Button(root)
         GButton_101["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=10)
         GButton_101["font"] = ft
         GButton_101["fg"] = "#000000"
         GButton_101["justify"] = "center"
-        GButton_101["text"] = "Connect"
+        GButton_101["text"] = "Conenct"
         GButton_101.place(x=470,y=160,width=70,height=25)
         GButton_101["command"] = self.GButton_101_command
 
+        # label for komunikacija area
         GLabel_520=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_520["font"] = ft
@@ -48,22 +53,7 @@ class App:
         GLabel_520["text"] = "Komunikacija"
         GLabel_520.place(x=470,y=20,width=70,height=25)
 
-        GLabel_689=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=10)
-        GLabel_689["font"] = ft
-        GLabel_689["fg"] = "#333333"
-        GLabel_689["justify"] = "center"
-        GLabel_689["text"] = ""
-        GLabel_689.place(x=370,y=10,width=70,height=25)
-
-        """GLabel_249=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=10)
-        GLabel_249["font"] = ft
-        GLabel_249["fg"] = "#333333"
-        GLabel_249["justify"] = "center"
-        GLabel_249["text"] = "______________________________________________________________________________________________________________"
-        GLabel_249.place(x=260,y=380,width=70,height=25)"""
-
+        # label for title Kamera 2 area
         GLabel_445=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_445["font"] = ft
@@ -72,14 +62,16 @@ class App:
         GLabel_445["text"] = "Kamera 2"
         GLabel_445.place(x=470,y=50,width=70,height=25)
 
+        # label for port dropdown menu in Kamera 2 area
         GLabel_647=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_647["font"] = ft
         GLabel_647["fg"] = "#333333"
         GLabel_647["justify"] = "center"
         GLabel_647["text"] = "Port"
-        GLabel_647.place(x=410,y=130,width=70,height=25)
+        GLabel_647.place(x=420,y=130,width=70,height=25)
 
+        # label for baudrate dropdown menu in kamera 2 area
         GLabel_848=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_848["font"] = ft
@@ -88,12 +80,14 @@ class App:
         GLabel_848["text"] = "Baudrate"
         GLabel_848.place(x=420,y=90,width=70,height=25)
 
+        # dropwdon menue in kamera 2 area
         self.baudrate_var = tk.StringVar()
         GCombobox_baudrate = ttk.Combobox(root, textvariable=self.baudrate_var, state='readonly')
         GCombobox_baudrate['values'] = ("9600", "4800")
         GCombobox_baudrate.current(0)  # set selection
         GCombobox_baudrate.place(x=500, y=260, width=80, height=25)
 
+        # label in kalibrator area - title
         GLabel_240=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_240["font"] = ft
@@ -102,6 +96,7 @@ class App:
         GLabel_240["text"] = "Kalibrator"
         GLabel_240.place(x=470,y=220,width=70,height=25)
 
+        # kalibrator area - title label
         GLabel_558=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_558["font"] = ft
@@ -110,6 +105,7 @@ class App:
         GLabel_558["text"] = "Baudrate"
         GLabel_558.place(x=410,y=260,width=70,height=25)
 
+        # Kalibrator area - label for port dropdown menu
         GLabel_647=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_647["font"] = ft
@@ -118,6 +114,7 @@ class App:
         GLabel_647["text"] = "Port"
         GLabel_647.place(x=410,y=300,width=70,height=25)
 
+        # kalibrator - connect button
         GButton_113=tk.Button(root)
         GButton_113["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=10)
@@ -136,6 +133,7 @@ class App:
         GListBox_956["justify"] = "center"
         GListBox_956.place(x=500,y=90,width=80,height=25)
         GListBox_956["selectmode"] = "extended"""
+        # kalibrator area - dropdown menu
         self.baudrate_var = tk.StringVar()
         GCombobox_baudrate = ttk.Combobox(root, textvariable=self.baudrate_var, state='readonly')
         GCombobox_baudrate['values'] = ("9600", "4800")
@@ -152,6 +150,8 @@ class App:
         GListBox_555["fg"] = "#333333"
         GListBox_555["justify"] = "center"
         GListBox_555.place(x=500,y=120,width=80,height=25)"""
+
+        # kamera 2 area - dropdown menu for port
         self.baudrate_var = tk.StringVar()
         GCombobox_baudrate = ttk.Combobox(root, textvariable=self.baudrate_var, state='readonly')
         GCombobox_baudrate['values'] = ("COM3", "COM4")
@@ -167,6 +167,8 @@ class App:
         GListBox_559["fg"] = "#333333"
         GListBox_559["justify"] = "center"
         GListBox_559.place(x=490,y=300,width=80,height=25)"""
+
+        # kalibrator area - dropdonw menu
         self.baudrate_var = tk.StringVar()
         GCombobox_baudrate = ttk.Combobox(root, textvariable=self.baudrate_var, state='readonly')
         GCombobox_baudrate['values'] = ("COM3", "COM4")
@@ -174,31 +176,34 @@ class App:
         GCombobox_baudrate.place(x=490,y=300,width=80,height=25)
         
 
-
-        GMessage_843=tk.Message(root)
+        # time
+        GLabel_843=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GMessage_843["font"] = ft
-        GMessage_843["fg"] = "#333333"
-        GMessage_843["justify"] = "center"
-        GMessage_843["text"] = "Time"
-        GMessage_843.place(x=460,y=410,width=80,height=25)
+        GLabel_843["font"] = ft
+        GLabel_843["fg"] = "#333333"
+        GLabel_843["anchor"] = "w" # Use anchor 'w' for left alignment within the provided space
+        GLabel_843["text"] = "Time"
+        GLabel_843.place(x=460,y=410,width=100,height=25)
 
-        GMessage_187=tk.Message(root)
+        # date
+        GLabel_187=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GMessage_187["font"] = ft
-        GMessage_187["fg"] = "#333333"
-        GMessage_187["justify"] = "center"
-        GMessage_187["text"] = "Date"
-        GMessage_187.place(x=460,y=440,width=80,height=25)
+        GLabel_187["font"] = ft
+        GLabel_187["fg"] = "#333333"
+        GLabel_187["anchor"] = "w" # Use anchor 'w' for left alignment within the provided space
+        GLabel_187["text"] = "Date"
+        GLabel_187.place(x=460,y=440,width=100,height=25)
 
-        GMessage_395=tk.Message(root)
+        # location 
+        GLabel_395=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
-        GMessage_395["font"] = ft
-        GMessage_395["fg"] = "#333333"
-        GMessage_395["justify"] = "center"
-        GMessage_395["text"] = "Lokacija: Ljubljana"
-        GMessage_395.place(x=480,y=460,width=80,height=25)
+        GLabel_395["font"] = ft
+        GLabel_395["fg"] = "#333333"
+        GLabel_395["anchor"] = "w" # Use anchor 'w' for left alignment within the provided space
+        GLabel_395["text"] = "Lokacija: Ljubljana"
+        GLabel_395.place(x=460,y=470,width=140,height=25)  # Increased width to prevent wrapping
 
+        # kalibracija area - area title
         GLabel_830=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_830["font"] = ft
@@ -207,6 +212,7 @@ class App:
         GLabel_830["text"] = "Kalibracija"
         GLabel_830.place(x=170,y=410,width=70,height=25)
 
+        # kalibracija area  - 
         GLabel_253=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         GLabel_253["font"] = ft
@@ -215,6 +221,7 @@ class App:
         GLabel_253["text"] = "Temperatura"
         GLabel_253.place(x=90,y=450,width=70,height=25)
 
+        # FIX IT
         GLineEdit_385=tk.Entry(root)
         GLineEdit_385["borderwidth"] = "1px"
         ft = tkFont.Font(family='Times',size=10)
